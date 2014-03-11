d3cd248401b60a4ecf5b4aee024ace6fce4de027
Jonah Back <backjo@mail.uc.edu>
2014-03-11 15:12:56 -0400

    Renamable organization support

--- controllers/settings.js
+++ controllers/settings.js
@@ -1,21 +1,24 @@
 var Settings = require('../models/Settings')
 var gCal = require('google-calendar')
+var app = require('../app');
+
+
 exports.getSettings = function(req, res) {
   Settings.findOne(function(err, settings){
-    console.log(settings.calendarKey);
-    var google_calendar = new gCal.GoogleCalendar(settings.calendarKey);
-    google_calendar.calendarList.list(function(err, calendarList) {
-      console.log(calendarList);
-      console.log(err);
-
-      google_calendar.events.list(calendarList.items[0].id, function(err, events) {
-        console.log(events);
-      });
+    res.render('settings', {
+      title: req.app.locals.organization,
+      settings: settings
     });
   });
+}
 
-
-  res.render('settings', {
-    title: 'Settings'
-  });
+exports.postSettings = function(req, res) {
+  Settings.findOne(function(err, settings) {
+    console.log(req.body);
+    settings.organizationName = req.body.organizationName;
+    settings.save();
+    req.app.locals.organization = req.body.organizationName;
+    console.log(req.app.locals);
+    res.redirect('/settings');
+  })
 }
--- models/Settings.js
+++ models/Settings.js
@@ -1,7 +1,8 @@
 var mongoose = require('mongoose');
 
 var settingsSchema = new mongoose.Schema({
-  calendarKey: {type: String, unique: true, default: ''}
+  calendarKey: {type: String, unique: true, default: ''},
+  organizationName: {type: String, default: 'Student Organization'}
 });
 
 module.exports = mongoose.model('Settings', settingsSchema);
