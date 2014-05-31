94d2f612c24c4d1417914b6e784b1e2d71466317
Jonah Back <backjo@mail.uc.edu>
2014-05-31 19:47:23 -0400

    Removed unnessecary assets

--- app.js
+++ app.js
@@ -36,8 +36,6 @@ refreshToken();
 
 var homeController = require('./controllers/home');
 var userController = require('./controllers/user');
-var apiController = require('./controllers/api');
-var contactController = require('./controllers/contact');
 var forgotController = require('./controllers/forgot');
 var resetController = require('./controllers/reset');
 
@@ -130,8 +128,9 @@ if (process.env.NODE_ENV !== 'production') {
       csrf: req.csrfToken()
     })
   })
+  app.use(express.errorHandler());
 }
-app.use(express.errorHandler());
+
 
 /**
  * Application routes.
