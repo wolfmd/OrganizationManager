5f9ece75e31da6d7251d3cbb4f7cbf325a30cd1c
Jonah Back <backjo@mail.uc.edu>
2014-05-31 19:51:41 -0400

    Missed dead references

--- app.js
+++ app.js
@@ -175,8 +175,6 @@ app.get('/reset/:token', resetController.getReset);
 app.post('/reset/:token', resetController.postReset);
 app.get('/signup',  userController.getSignup);
 app.post('/signup',  userController.postSignup);
-app.get('/contact', contactController.getContact);
-app.post('/contact', contactController.postContact);
 app.get('/account', passportConf.isAuthenticated, userController.getAccount);
 app.post('/account/profile', passportConf.isAuthenticated, userController.postUpdateProfile);
 app.post('/account/password', passportConf.isAuthenticated, userController.postUpdatePassword);
